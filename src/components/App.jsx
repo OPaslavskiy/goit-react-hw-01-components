9fe5277ae0874ac98003638799a82335393e3ab4
OPaslavskiy <bankir.op@gmail.com>
2023-02-18 17:45:56 +0200

    q

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import { ProfileList } from './profile/ProfileList';
 import { Statistics } from './statistics/Statistics';
 import { FriendList } from './friendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout';
 import user from '../jsonFile/user.json';
 import data from '../jsonFile/data.json';
 import friends from '../jsonFile/friends.json';
 import transactions from '../jsonFile/transactions.json';
-import { GlobalStyle } from './GlobalStyle';
-import { Layout } from './Layout';
 
 export const App = () => {
   return (
